d4414f32e29ec11c568bbb71877ef1ca540fa05f
ambujs89 <ambujs89@gmail.com>
2022-06-20 03:38:20 +0530

    Exercise 1.2 complete

--- Part-1/courseinfo/src/App.js
+++ Part-1/courseinfo/src/App.js
@@ -1,8 +1,47 @@
+const course = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
 
+
+const Header = (props) => {
+  return (
+    <><h1>{props.course}</h1></>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <><p>{props.part} : {props.exercise} </p></>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </div>
+  )
+}
+
+const Total = () => {
+  return (
+    <><p>Nmber of exercises = {exercises1 + exercises2 + exercises3}</p></>
+  )
+}
 
 function App() {
   return (
-    <><p>Hello World!</p></>
+    <>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </>
   );
 }
 
