c0efe5708e1a1c591f18bb03ff39a945400964d5
ambujs89 <ambujs89@gmail.com>
2022-06-20 13:24:44 +0530

    Exercise 1.7 Step 2

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,6 +1,35 @@
+import { useState } from 'react'
+
+const Heading = ({ text }) => (<h1>{text}</h1>)
+const Button = ({handleClick, text}) => (<button onClick={handleClick}>{text}</button>)
+const Display = ({text, variable}) => (<div>{text} : {variable}</div>)
 
 function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+
+  console.log(good)
+
+  return (
+  <>
+    <Heading text="Give FeedBack" />
+
+    <Button handleClick={() => setGood(good + 1)} text = "Good" />
+    <Button handleClick={() => setNeutral(neutral + 1)} text = "Neutral" />
+    <Button handleClick={() => setBad(bad + 1)} text = "Bad" />
+
+    <Heading text="Statistics" />
+
+    <Display text="Good" variable={good} />
+    <Display text="Neutral" variable={neutral} />
+    <Display text="Bad" variable={bad} />
+    <Display text="Total" variable={good + bad + neutral} />
+    <Display text="Average" variable={(good - bad) / (good+bad+neutral)} />
+    <Display text="Positive" variable={(good - bad) / (good+bad+neutral) * 100 + "%"} />
+  </>
+  )
 }
 
 export default App;
