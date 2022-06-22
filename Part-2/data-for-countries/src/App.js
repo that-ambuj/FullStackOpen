0f17ca898b53445b0b9d3ed0f4c68e7f6e5c71e0
ambujs89 <ambujs89@gmail.com>
2022-06-22 22:27:21 +0530

    exercise 2.11

--- Part-2/data-for-countries/src/App.js
+++ Part-2/data-for-countries/src/App.js
@@ -1,10 +1,98 @@
+import { useState, useEffect, Component } from "react";
+import axios from "axios";
+
+import searchInObjArr from "./components/searchInObjArr";
+import InputField from "./components/InputField";
+
+const DisplayNames = (props) => {
+    const { namesArr } = props;
+
+    if (namesArr.length > 10) {
+        return <div>Please enter more letters</div>;
+    }
+
+    return namesArr.map((name) => <div>{name}</div>);
+};
+
+const ShowButton = () => {};
+
+const DisplayCountry = (props) => {
+    const countryArr = props.countryArr
+    const [ countryObj ] = props.countryArr;
+    const countryName = countryObj.name.common;
+    const capital = countryObj.capital[0];
+    const area = countryObj.area;
+    const languagesArr = Object.entries(countryObj.languages);
+    const flagSVG = countryObj.flags.svg;
+
+    const [svg, setSVG] = useState(<svg></svg>);
+
+    useEffect(() => {
+        axios.get(flagSVG).then((response) => {
+            setSVG(response.data);
+        });
+    });
+
+    if(countryArr.length > 1) {
+        return <DisplayNames
+            namesArr={countryArr.map(country => country.name.common)}
+            key={countryArr.map((obj) => obj.ccn3)}
+        />
+    }
+
+    return (
+        <>
+            <h2>{countryName}</h2>
+            <div>Capital : {capital}</div>
+            <div>Area : {area}</div>
+
+            <h4>Languages :</h4>
+            <ul>
+                {languagesArr.map((lang) => (
+                    <li key={lang[0]}> {lang[1]}</li>
+                ))}
+            </ul>
+
+            <div dangerouslySetInnerHTML={{ __html: svg }} />
+        </>
+    );
+};
 
 function App() {
-  return (
-    <div>
+    const [countrySearch, setCountrySearch] = useState("");
+    const [displayContent, setDisplayContent] = useState([]);
+    const [countriesData, setCountriesData] = useState([]);
 
-    </div>
-  );
+    useEffect(() => {
+        axios.get("https://restcountries.com/v3.1/all").then((response) => {
+            setCountriesData(response.data);
+        });
+    }, []);
+
+    const searchRegex = new RegExp(countrySearch, "ig");
+
+    const searchResultNames =
+        { countrySearch } !== ""
+            ? countriesData
+                  .map((countries) => countries.name.common)
+                  .filter((names) => searchRegex.test(names))
+            : countriesData;
+
+    const resultObjArr = searchResultNames
+        .map((result) => searchInObjArr(countriesData, result))
+        .flat();
+
+    return (
+        <div>
+            <InputField
+                text="find countries"
+                value={countrySearch}
+                onChange={(event) => setCountrySearch(event.target.value)}
+            />
+            
+            <DisplayCountry countryArr={resultObjArr} />
+        </div>
+    );
 }
 
 export default App;
--- Part-2/data-for-countries/src/components/searchInObjArr.js
+++ Part-2/data-for-countries/src/components/searchInObjArr.js
@@ -1,12 +1,13 @@
-const searchInObjArr = ( objArr, term, prop) => {
-  const searchRegex = new RegExp(term, 'ig')
-  const newArr = [...objArr]
+const searchInObjArr = (objArr, term) => {
+    const searchRegex = new RegExp(term, "ig");
+    const newArr = [...objArr];
 
-  const searchResults = {term} !== "" 
-  ? newArr.filter(target => searchRegex.test(target[prop]))
-  : newArr
+    const searchResults =
+        { term } !== ""
+            ? newArr.filter((target) => searchRegex.test(target.name.common))
+            : newArr;
 
-  return (searchResults)
-}
+    return searchResults;
+};
 
-export default searchInObjArr
+export default searchInObjArr;
