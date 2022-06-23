e79412fd2d1515a58284902ac049315e4a6e0087
ambujs89 <ambujs89@gmail.com>
2022-06-23 10:26:20 +0530

    Exercise 2.11 search enabled/fixed

--- Part-2/data-for-countries/src/App.js
+++ Part-2/data-for-countries/src/App.js
@@ -1,44 +1,23 @@
-import { useState, useEffect, Component } from "react";
-import axios from "axios";
+import { useState, useEffect, Component } from "react"
+import axios from "axios"
+import nanoid  from 'nanoid'
 
-import searchInObjArr from "./components/searchInObjArr";
-import InputField from "./components/InputField";
+import searchInObjArr from "./components/searchInObjArr"
+import InputField from "./components/InputField"
 
-const DisplayNames = (props) => {
-    const { namesArr } = props;
+const DisplayNames = ({ name }) => {
+    return <div>{name}</div>
+}
 
-    if (namesArr.length > 10) {
-        return <div>Please enter more letters</div>;
-    }
-
-    return namesArr.map((name) => <div>{name}</div>);
-};
-
-const ShowButton = () => {};
+const ShowButton = () => {}
 
 const DisplayCountry = (props) => {
-    const countryArr = props.countryArr
-    const [ countryObj ] = props.countryArr;
-    const countryName = countryObj.name.common;
-    const capital = countryObj.capital[0];
-    const area = countryObj.area;
-    const languagesArr = Object.entries(countryObj.languages);
-    const flagSVG = countryObj.flags.svg;
-
-    const [svg, setSVG] = useState(<svg></svg>);
-
-    useEffect(() => {
-        axios.get(flagSVG).then((response) => {
-            setSVG(response.data);
-        });
-    });
-
-    if(countryArr.length > 1) {
-        return <DisplayNames
-            namesArr={countryArr.map(country => country.name.common)}
-            key={countryArr.map((obj) => obj.ccn3)}
-        />
-    }
+    const countryObj = props.countryObj
+    const countryName = countryObj.name.common
+    const capital = countryObj.capital[0]
+    const area = countryObj.area
+    const languagesArr = Object.entries(countryObj.languages)
+    const flagSVG = countryObj.flags.png
 
     return (
         <>
@@ -53,46 +32,76 @@ const DisplayCountry = (props) => {
                 ))}
             </ul>
 
-            <div dangerouslySetInnerHTML={{ __html: svg }} />
+            <img src={flagSVG} alt="flag" />
         </>
-    );
-};
+    )
+}
 
 function App() {
-    const [countrySearch, setCountrySearch] = useState("");
-    const [displayContent, setDisplayContent] = useState([]);
-    const [countriesData, setCountriesData] = useState([]);
+    const [countrySearch, setCountrySearch] = useState("")
+    const [displayContent, setDisplayContent] = useState([])
+    const [countriesData, setCountriesData] = useState([])
 
     useEffect(() => {
-        axios.get("https://restcountries.com/v3.1/all").then((response) => {
-            setCountriesData(response.data);
-        });
-    }, []);
+        axios
+            .get(
+                "https://restcountries.com/v3.1/all?fields=name,capital,languages,area,flags,ccn3"
+            )
+            .then((response) => {
+                setCountriesData(response.data)
+            })
+    }, [])
 
-    const searchRegex = new RegExp(countrySearch, "ig");
+    const searchRegex = new RegExp(countrySearch, "i")
 
     const searchResultNames =
         { countrySearch } !== ""
             ? countriesData
                   .map((countries) => countries.name.common)
                   .filter((names) => searchRegex.test(names))
-            : countriesData;
+            : countriesData
 
     const resultObjArr = searchResultNames
         .map((result) => searchInObjArr(countriesData, result))
-        .flat();
+        .flat()
+
+    function checkResult(arr) {
+        if (arr.length < 1) {
+            return <div>No result found.</div>
+        }
+
+        if (arr.length > 10) {
+            return <div> Please enter more letters.</div>
+        }
+        if (arr.length <= 10 && arr.length > 1) {
+            return (
+                <div>
+                    {arr
+                        .map((country) => country.name)
+                        .map((name, index) => (
+                            <div key={ index }>{name.common}</div>
+                        ))}
+
+                    {console.log(arr)}
+                </div>
+            )
+        }
+
+        if (arr.length === 1) {
+            return <div>{<DisplayCountry countryObj={arr[0]} />}</div>
+        }
+    }
 
     return (
-        <div>
+        <>
             <InputField
                 text="find countries"
                 value={countrySearch}
                 onChange={(event) => setCountrySearch(event.target.value)}
             />
-            
-            <DisplayCountry countryArr={resultObjArr} />
-        </div>
-    );
+            {checkResult(resultObjArr)}
+        </>
+    )
 }
 
-export default App;
+export default App
