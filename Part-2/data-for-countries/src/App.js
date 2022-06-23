f7980ca30936a0c6ca55af6f8b7366662c6e901e
ambujs89 <ambujs89@gmail.com>
2022-06-23 22:11:34 +0530

    weather feature added

--- Part-2/data-for-countries/src/App.js
+++ Part-2/data-for-countries/src/App.js
@@ -1,123 +1,150 @@
-import { useState, useEffect, Component } from "react"
+import { useState, useEffect } from "react"
 import axios from "axios"
 
-import searchInObjArr from "./components/searchInObjArr"
 import InputField from "./components/InputField"
 
-const DisplayNames = ({ name }) => {
-  return <div>{name}</div>
-}
-
-const ShowButton = () => {}
+const api_key = process.env.REACT_APP_API_KEY
 
 const DisplayCountry = (props) => {
-  const countryObj = props.countryObj
-  const countryName = countryObj.name.common
-  const capital = countryObj.capital[0]
-  const area = countryObj.area
-  const languagesArr = Object.entries(countryObj.languages)
-  const flagSVG = countryObj.flags.png
-  console.log(countryObj)
+   const [weather, setWeather] = useState("")
+   const [icon, setIcon] = useState("")
 
-  return (
-    <>
-      <h2>{countryName}</h2>
-      <div>Capital : {capital}</div>
-      <div>Area : {area}</div>
+   const countryObj = props.countryObj
+   const countryName = countryObj.name.common
+   const capital = countryObj.capital[0]
+   const area = countryObj.area
+   const languagesArr = Object.entries(countryObj.languages)
+   const flagSVG = countryObj.flags.png
 
-      <h4>Languages :</h4>
-      <ul>
-        {languagesArr.map((lang) => (
-          <li key={lang[0]}> {lang[1]}</li>
-        ))}
-      </ul>
+   const lat = countryObj.capitalInfo.latlng[0]
+   const lng = countryObj.capitalInfo.latlng[1]
 
-      <img src={flagSVG} alt="flag" />
-    </>
-  )
+   useEffect(() => {
+      axios
+         .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}&units=metric`
+         )
+         .then((response) => {
+            setWeather(response.data)
+            setIcon(response.data.weather[0].icon)
+         })
+   }, [])
+
+   console.log(weather)
+
+   if (weather.hasOwnProperty("main")) {
+      const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+      const temp = weather.main.temp
+      const wind = weather.wind.speed
+
+      return (
+         <>
+            <h2>{countryName}</h2>
+            <div>Capital : {capital}</div>
+            <div>Area : {area}</div>
+
+            <h4>Languages :</h4>
+            <ul>
+               {languagesArr.map((lang) => (
+                  <li key={lang[0]}> {lang[1]}</li>
+               ))}
+            </ul>
+
+            <img src={flagSVG} alt="flag" />
+
+            <h3>Weather in {capital} :</h3>
+            <div>Temperature : {temp} celsius</div>
+            <img src={iconURL} alt="weather icon" />
+            <div>Wind : {wind} m/s </div>
+         </>
+      )
+   }
 }
 
 const App = () => {
-  const [countrySearch, setCountrySearch] = useState("")
-  const [showToggle, setShowToggle] = useState(false)
-  const [countriesData, setCountriesData] = useState([])
-  const [showID, setShowID] = useState(0)
+   const [countrySearch, setCountrySearch] = useState("")
+   const [countriesData, setCountriesData] = useState([])
+   const [showID, setShowID] = useState(0)
 
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      const result = response.data
-      setCountriesData(result)
-    })
-  }, [])
+   useEffect(() => {
+      axios.get("https://restcountries.com/v3.1/all").then((response) => {
+         const result = response.data
+         setCountriesData(result)
+      })
+   }, [])
 
-  const searchRegex = new RegExp(countrySearch, "i")
+   const searchRegex = new RegExp(countrySearch, "i")
 
-  const searchResultNames =
-    { countrySearch } !== ""
-      ? countriesData
-          .map((countries) => countries.name.common)
-          .filter((names) => searchRegex.test(names))
-      : countriesData
+   const searchResultNames =
+      { countrySearch } !== ""
+         ? countriesData
+              .map((countries) => countries.name.common)
+              .filter((names) => searchRegex.test(names))
+         : countriesData
 
-  const resultObjArr = searchResultNames
-    .map((name) =>
-      countriesData.flat().filter((country) => country.name.common == name)
-    )
-    .flat()
-
-  function checkResult(arr) {
-    if (showID !== 0) {
-      const country = arr.filter((country) => country.ccn3 === showID)[0]
-
-      return (
-        <div>
-          <button onClick={() => setShowID(0)}>hide</button>
-          <DisplayCountry countryObj={country} key={country.ccn3} />
-        </div>
+   const resultObjArr = searchResultNames
+      .map((name) =>
+         countriesData.flat().filter((country) => country.name.common === name)
       )
-    }
+      .flat()
 
-    if (arr.length < 1) {
-      return <div>No result found.</div>
-    }
+   function checkResult(arr) {
+      if (showID !== 0) {
+         const country = arr.filter((country) => country.ccn3 === showID)[0]
 
-    if (arr.length > 10) {
-      return <div> Please enter more letters.</div>
-    }
-    if (arr.length <= 10 && arr.length > 1) {
-      return (
-        <div>
-          {arr
-            .map((country) => [country.name, country.ccn3])
-            .map((nameCode) => (
-              <>
-                <div key={nameCode[1]} id={nameCode[1]}>
-                  {nameCode[0].common}
-                  <button onClick={() => setShowID(nameCode[1])}> show</button>
-                </div>
-              </>
-            ))}
-        </div>
-      )
-    }
+         return (
+            <div>
+               <button onClick={() => setShowID(0)}>hide</button>
+               <DisplayCountry countryObj={country} key={country.ccn3} />
+            </div>
+         )
+      }
 
-    if (arr.length === 1) {
-      return (
-        <div>{<DisplayCountry countryObj={arr[0]} key={arr[0].ccn3} />}</div>
-      )
-    }
-  }
+      if (arr.length < 1) {
+         return <div>No result found.</div>
+      }
 
-  return (
-    <>
-      <InputField
-        text="find countries"
-        value={countrySearch}
-        onChange={(event) => setCountrySearch(event.target.value)}
-      />
-      {checkResult(resultObjArr)}
-    </>
-  )
+      if (arr.length > 10) {
+         return <div> Please enter more letters.</div>
+      }
+      if (arr.length <= 10 && arr.length > 1) {
+         return (
+            <div>
+               {arr
+                  .map((country) => [country.name, country.ccn3])
+                  .map((nameCode) => (
+                     <>
+                        <div key={nameCode[1]}>
+                           {nameCode[0].common}
+                           <button onClick={() => setShowID(nameCode[1])}>
+                              show
+                           </button>
+                        </div>
+                     </>
+                  ))}
+            </div>
+         )
+      }
+
+      if (arr.length === 1) {
+         return (
+            <div>
+               {<DisplayCountry countryObj={arr[0]} key={arr[0].ccn3} />}
+            </div>
+         )
+      }
+   }
+
+   return (
+      <>
+         <InputField
+            text="find countries"
+            value={countrySearch}
+            onChange={(event) => setCountrySearch(event.target.value)}
+         />
+         {checkResult(resultObjArr)}
+      </>
+   )
 }
 
 export default App
