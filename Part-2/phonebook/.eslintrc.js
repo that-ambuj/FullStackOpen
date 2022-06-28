b473adcade6850586a60750a6581683d68525982
ambujs89 <ambujs89@gmail.com>
2022-06-28 10:38:25 +0530

    integrate eslint

---
+++ Part-2/phonebook/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+   "env": {
+      "es2021": true,
+      "node": true
+   },
+   "extends": "eslint:recommended",
+   "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+   },
+   "rules": {
+      "indent": [
+         "error",
+         3
+      ],
+      "linebreak-style": [
+         "error",
+         "windows"
+      ],
+      "quotes": [
+         "error",
+         "double"
+      ],
+      "semi": [
+         "error",
+         "never"
+      ],
+   }
+}
