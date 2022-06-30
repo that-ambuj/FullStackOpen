11ba286fef2a1db6462fbd1bf73b046993c37e05
ambujs89 <ambujs89@gmail.com>
2022-07-01 00:55:55 +0530

    added put, delete and get by id in api

--- Part-4/bloglist/controllers/blogs.js
+++ Part-4/bloglist/controllers/blogs.js
@@ -1,18 +1,44 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 
-blogsRouter.get("/", (request, response) => {
-   Blog.find({}).then((blogs) => {
-      response.json(blogs)
-   })
+blogsRouter.get("/", async (request, response) => {
+   const blogs = await Blog.find({})
+
+   response.json(blogs)
 })
 
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
    const blog = new Blog(request.body)
 
-   blog.save().then((result) => {
-      response.status(201).json(result)
-   }).catch(error => next(error))
+   const savedBlog = await blog.save()
+
+   response.status(201).json(savedBlog)
 })
+
+blogsRouter.get("/:id", async (request, response) => {
+   const blog = await Blog.findById(request.params.id)
+      
+   response.json(blog)
+})
+
+blogsRouter.delete("/:id", async (request, response) => {
+   await Blog.findByIdAndDelete(request.params.id)
+   
+   response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+   const body = request.body
+ 
+   const blog = {
+     title : body.title,
+     author : body.author,
+     likes : body.likes
+   }
+ 
+   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+   
+   response.json(updatedBlog)
+ })
 
 module.exports = blogsRouter
