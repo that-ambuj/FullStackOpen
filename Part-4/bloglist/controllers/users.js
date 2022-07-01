ca6d848f44cd428bbdd6846a10217394d47f7d73
ambujs89 <ambujs89@gmail.com>
2022-07-01 20:44:51 +0530

    added password validation in POST route

--- Part-4/bloglist/controllers/users.js
+++ Part-4/bloglist/controllers/users.js
@@ -1,6 +1,16 @@
 const usersRouter = require("express").Router()
 const User = require("../models/user")
 const bcrypt = require("bcrypt")
+const passwordValidator = require("password-validator")
+
+const passwordSchema = new passwordValidator()
+                                       .is().min(8)
+                                       .has().lowercase()
+                                       .has().digits(2)
+                                       .has().symbols()
+                                       .has().not().spaces()
+
+   
 
 usersRouter.get("/", async (request, response) => {
    const users = await User.find({})
@@ -12,23 +22,36 @@ usersRouter.post("/", async (request, response) => {
    const { username, name, password } = request.body
 
    const existingUser = await User.findOne({ username })
+
    if (existingUser) {
-     return response.status(400).json({
-       error: 'username must be unique'
-     })
+      return response.status(400).json({
+         error: "username must be unique",
+      })
    }
- 
+
+   const passwordIsValid = await passwordSchema.validate(password)
+
+   console.log(passwordIsValid)
+
+   if (passwordIsValid !== true) {
+      return response
+         .status(406)
+         .json({
+            error: "password must have at least 8 characters, including digits and symbols.",
+         })
+   }
+
    const saltRounds = 10
    const passwordHash = await bcrypt.hash(password, saltRounds)
- 
+
    const user = new User({
-     username,
-     name,
-     passwordHash,
+      username,
+      name,
+      passwordHash,
    })
- 
+
    const savedUser = await user.save()
- 
+
    response.status(201).json(savedUser)
 })
 
