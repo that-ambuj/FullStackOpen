5b3fa94a91085e4cbc524a233e59b8375acd685e
ambujs89 <ambujs89@gmail.com>
2022-07-01 20:20:39 +0530

    add minLength validator

--- Part-4/bloglist/models/user.js
+++ Part-4/bloglist/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-   username: { type: String, required: true },
+   username: { type: String, minLength : 3, required: true },
    name: { type: String, required: true },
    passwordHash: { type: String, required: true },
 })
