bbc35ca7ed90a71ae0ef14cc4f7db0f6d32ca15f
ambujs89 <ambujs89@gmail.com>
2022-07-01 21:20:43 +0530

    user in blogs will populate on GET

--- Part-4/bloglist/controllers/blogs.js
+++ Part-4/bloglist/controllers/blogs.js
@@ -4,7 +4,7 @@ const Blog = require("../models/blog")
 const User = require("../models/user")
 
 blogsRouter.get("/", async (request, response) => {
-   const blogs = await Blog.find({})
+   const blogs = await Blog.find({}).populate('user')
 
    response.json(blogs)
 })
