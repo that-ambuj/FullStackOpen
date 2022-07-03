0b901e86815796fe7bf5785bed2d6a921e4cfaed
ambujs89 <ambujs89@gmail.com>
2022-07-03 09:00:45 +0530

    updated routing for token extractor

--- Part-4/bloglist/app.js
+++ Part-4/bloglist/app.js
@@ -5,9 +5,10 @@ const app = express()
 const cors = require("cors")
 // import express async await error handler 
 require('express-async-errors')
+
 // import routers
-const loginRouter = require("./controllers/login")
 const blogsRouter = require("./controllers/blogs")
+const loginRouter = require("./controllers/login")
 const usersRouter = require("./controllers/users")
 // import middleware for logging, error handling and database
 const middleware = require("./utils/middleware")
@@ -31,12 +32,13 @@ app.use(cors())
 app.use(express.json())
 // use request logging middleware
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 // setup routers
+app.use("/api/blogs", blogsRouter)
 app.use("/api/login", loginRouter)
 app.use("/api/users", usersRouter)
-app.use("/api/blogs", blogsRouter)
 // use custom error handling middleware
-app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
