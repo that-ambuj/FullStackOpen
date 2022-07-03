4bb4cdbaa391774fd9f905b0888830443f0e2188
ambujs89 <ambujs89@gmail.com>
2022-07-03 09:00:31 +0530

    added tokenExtractor with next() token req passing

--- Part-4/bloglist/utils/middleware.js
+++ Part-4/bloglist/utils/middleware.js
@@ -4,15 +4,26 @@ const requestLogger = (request, response, next) => {
    logger.info("Method:", request.method)
    logger.info("Path:  ", request.path)
 
-   if (!(request.path === "/api/users" && request.method === "POST")) {
+   if (!(request.path === "/api/users" || "/api/blogs" && request.method === "POST")) {
       logger.info("Body:  ", request.body)
    }
    logger.info("---")
    next()
 }
 
-const unknownEndpoint = (request, response) => {
-   response.status(404).send({ error: "unknown endpoint" })
+const tokenExtractor = async (request, response, next) => {
+   const getTokenFrom = (request, response, next) => {
+      const authorization = request.get("authorization")
+      if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+         return authorization.substring(7)
+      }
+      return null
+   }
+
+   const token = await getTokenFrom(request)
+
+   request.token = token
+   next()
 }
 
 const errorHandler = (error, request, response, next) => {
@@ -22,12 +33,25 @@ const errorHandler = (error, request, response, next) => {
       return response.status(400).send({ error: "malformatted id" })
    } else if (error.name === "ValidationError") {
       return response.status(400).json({ error: error.message })
+   } else if (error.name === "JsonWebTokenError") {
+      return response.status(401).json({
+         error: "invalid token",
+      })
+   } else if (error.name === "TokenExpiredError") {
+      return response.status(401).json({
+         error: "token expired",
+      })
    }
 
    next(error)
 }
 
+const unknownEndpoint = (request, response) => {
+   response.status(404).send({ error: "unknown endpoint" })
+}
+
 module.exports = {
+   tokenExtractor,
    requestLogger,
    unknownEndpoint,
    errorHandler,
