5a5ff924758e5d1610d7d171a9dcc1ca893c24f3
ambujs89 <ambujs89@gmail.com>
2022-07-03 09:03:24 +0530

    login : added token generation for 24 h

--- Part-4/bloglist/controllers/login.js
+++ Part-4/bloglist/controllers/login.js
@@ -2,11 +2,16 @@ const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 const loginRouter = require("express").Router()
 const User = require("../models/user")
-const { SECRET } = require("../utils/config")
 
 loginRouter.post("/", async (request, response) => {
    const { username, password } = request.body
 
+   if(password == null || username == null) {
+      return response.status(401).json({
+         error : "please enter a username or password"
+      })
+   }
+   
    const user = await User.findOne({ username })
    const passwordCorrect =
       user === null ? false : await bcrypt.compare(password, user.passwordHash)
@@ -22,11 +27,14 @@ loginRouter.post("/", async (request, response) => {
       id: user._id,
    }
 
-   const token = jwt.sign(userForToken, SECRET)
+   const token = jwt.sign(userForToken, process.env.SECRET,{expiresIn : "24 hours"} )
 
    response
       .status(200)
       .send({ token, username: user.username, name: user.name })
 })
 
+
+
 module.exports = loginRouter
+
