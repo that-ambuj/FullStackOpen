3d688dc507fb057f3626c86d321d09368a9c56b6
Ambuj Singh <ambujs89@gmail.com>
2022-07-20 16:02:21 +0530

    added show/hide button for Info

--- Part-5/bloglist-frontend/src/components/BlogItem/BlogItem.js
+++ Part-5/bloglist-frontend/src/components/BlogItem/BlogItem.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
-const BlogItem = ({ blog }) => (
-    <div className='box'>
-        <div className='title'>{blog.title}</div>
-        <div className='author'>{blog.author}</div>
-    </div>
-)
+const Info = ({ blog, likeHandler }) => {
+    return (
+        <>
+            <div>{blog.url}</div>
+            <div>Likes : {blog.likes}</div>
+            <button onSubmit={likeHandler}> Like </button>
+        </>
+    )
+}
+const BlogItem = ({ blog }) => {
+    const [visible, setVisible] = useState(false)
+    const addLike = () => {}
 
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+    return (
+        <div className='box'>
+            <div className='title'>{blog.title}</div>
+            <div className='author'>{blog.author}</div>
+            <button onClick={toggleVisibility}>{visible ? 'hide' : 'show'}</button>
+            {visible && <Info blog={blog} likeHandler={addLike} />}
+        </div>
+    )
+}
 export default BlogItem
