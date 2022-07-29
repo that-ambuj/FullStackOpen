fcf4eb199636a766ffd67f2e3eaaad2c40a6a191
Ambuj Singh <ambujs89@gmail.com>
2022-07-29 22:10:31 +0530

    added all functionality from base repo

---
+++ Part-6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,50 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('zero sets everything to zero', () => { 
+    const state = {
+      good: 6,
+      bad: 9,
+      ok: 20
+    }
+    const action = {
+      type: 'ZERO'
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
+   })
+})
