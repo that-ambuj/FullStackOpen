fd3b52b1d9617ef5a5aeeaeebec4edfe94fa1e61
Ambuj Singh <ambujs89@gmail.com>
2022-08-08 00:02:44 +0530

    extracted components

--- Part-6/redux-anecdotes/src/App.js
+++ Part-6/redux-anecdotes/src/App.js
@@ -1,40 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { upvoteAnec, createAnec } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdotesList from './components/AnecdotesList'
 
 const App = () => {
-    const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
-    const dispatch = useDispatch()
-
-    const addAnec = event => {
-        event.preventDefault()
-
-        const content = event.target.anec.value
-        event.target.anec.value = ''
-
-        if (content !== '') {
-            dispatch(createAnec(content))
-        }
-    }
-
     return (
         <div>
-            <h2>Anecdotes</h2>
-            {anecdotes.map(anecdote => (
-                <div key={anecdote.id}>
-                    <div>{anecdote.content}</div>
-                    <div>
-                        has {anecdote.votes}
-                        <button onClick={() => dispatch(upvoteAnec(anecdote.id))}>vote</button>
-                    </div>
-                </div>
-            ))}
-            <h2>create new</h2>
-            <form onSubmit={addAnec}>
-                <div>
-                    <input name='anec' />
-                </div>
-                <button>create</button>
-            </form>
+            <AnecdotesList />
+            <AnecdoteForm />
         </div>
     )
 }
