d8f872f56bcca8165c9507ecf9444a2e87fbdf5e
Ambuj Singh <ambujs89@gmail.com>
2022-08-08 00:02:59 +0530

    extracted form component

---
+++ Part-6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { createAnec } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+
+    const addAnec = event => {
+        event.preventDefault()
+
+        const content = event.target.anec.value
+        event.target.anec.value = ''
+
+        if (content !== '') {
+            dispatch(createAnec(content))
+        }
+    }
+
+    return (
+        <>
+            <h2>create new</h2>
+            <form onSubmit={addAnec}>
+                <div>
+                    <input name='anec' />
+                </div>
+                <button>create</button>
+            </form>
+        </>
+    )
+}
+
+export default AnecdoteForm
